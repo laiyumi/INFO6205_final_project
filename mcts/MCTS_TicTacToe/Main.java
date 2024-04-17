cdbd4432aa7b073ed02f0736c75b1b9a6c82740f
Nangongnuanshan <guo.ting1@northeastern.edu>
2024-04-17 18:24:12 -0400

    runTime

--- mcts/MCTS_TicTacToe/Main.java
+++ mcts/MCTS_TicTacToe/Main.java
@@ -1,13 +1,15 @@
-package edu.neu.coe.info6205.mcts.MCTS_TicTacToe;
+package mcts.MCTS_TicTacToe;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static final int NUM_GAMES = 2;
 
     public static void main(String[] args) {
 
-        boolean human_player = true;
+        boolean human_player = false;
 
         int p1Wins = 0;
         int p2Wins = 0;
@@ -21,16 +23,22 @@ public class Main {
             Board board = new Board(6,7); // Connect 4 is usually played 6 rows by 7 columns
             int player = Board.P1;
             int totalMoves = row * col;
-            
+            //Map<Integer, Double> columnWinRates = mcts.findNextMoveWinRate(board, Board.P1);
+
             // use mcts to do the move
             for (int i = 0; i < totalMoves; i++) {
 
                 if (player == Board.P1) {
+                    long startTime = System.nanoTime();
                     board = mcts.findNextMove(board, player);
+                    long endTime = System.nanoTime();
+
+                    long duration = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
                     //System.out.println("move: " + i);
                     System.out.println("\n" + "Board after move: " + i);
                     System.out.println("0 1 2 3 4 5 6");
                     board.printBoard();
+                    System.out.println("Time taken for move: " + duration + " ms");
                 }
                 else {
                     if (human_player) {
@@ -48,6 +56,17 @@ public class Main {
                         }
                         //System.out.println("------------------");
                         board.printBoard();
+                    }else{
+                        long startTime = System.nanoTime();
+                        board = mcts.findNextMove(board, player);
+                        long endTime = System.nanoTime();
+
+                        long duration = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+                        //System.out.println("move: " + i);
+                        System.out.println("\n" + "Board after move: " + i);
+                        System.out.println("0 1 2 3 4 5 6");
+                        board.printBoard();
+                        System.out.println("Time taken for move: " + duration + " ms");
                     }
                 }
 
